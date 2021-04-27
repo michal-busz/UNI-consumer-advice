9dd3110b614a041f926ec9c8aa37e51bbc540e2c
Michal Busz <michal@busz.it>
2021-04-27 16:30:12 +0100

    improvments

---
+++ questions/question2.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+
+Your choice was no. <?php echo $_POST["option"]; ?><br>
+
+
+</body>
+</html>
--- questions/template.php
+++ questions/template.php
@@ -22,17 +22,34 @@ include_once("../sidebar.php");
 <body>
 <!-- Main content-->
 <div class="content-main" style="margin-left:250px">
-    <a href="https://www.google.com"><img src="../static/image.jpg" alt="image description ""></a>
+    <br><br><br><br><br><br>
 
-    <form action="question2.php" method="post">
-        Name: <input type="radio"  name="name"><br>
-        Name1: <input type="radio"  name="name"><br>
-        Name2: <input type="radio"  name="name"><br>
-        Name3: <input type="radio"  name="name"><br>
-        Name4: <input type="radio" name="name"><br>
-        <input type="submit">
+    <table style="width:100%">
+        <form action="question2.php" method="post"
+              <tr>
+                  <th><img src="../static/image.jpg" height="50px" width="50px" alt="image description ""></th> <!-- Option 1-->
+                  <th><img src="../static/image.jpg" height="50px" width="50px" alt="image description ""></th>  <!-- Option 2-->
+                  <th><img src="../static/image.jpg" height="50px" width="50px" alt="image description ""></th> <!-- Option 3-->
+                  <th><img src="../static/image.jpg" height="50px" width="50px" alt="image description ""></th>  <!-- Option 4-->
+                  <th><img src="../static/image.jpg" height="50px" width="50px" alt="image description ""></th> <!-- Option 5-->
+              </tr>
+        <tr>
+            <th>
+                <button type="submit" name="option" value="1">Option 1</button>
+            </th>
+            <th>
+                <button type="submit" name="option" value="2">Option 2</button>
+            </th><th>
+                <button type="submit" name="option" value="3">Option 3</button>
+            </th><th>
+                <button type="submit" name="option" value="4">Option 4</button>
+            </th>
+            <th>
+                <button type="submit" name="option" value="5">Option 5</button>
+            </th>
     </form>
-
+        </tr>
+    </table>
     <?php
     include_once("../footer.php");
     ?>
