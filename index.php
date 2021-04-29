f471b68263319ef0fa816f7bdd042caee5d7fab9
Michal Busz <michal@busz.it>
2021-04-29 16:26:00 +0100

    images included

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>Customer advice</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="w3.css">
+<link rel="stylesheet" href="w3-black.css">
+<style>
+html,body,h1,h2,h3,h4,h5,h6 { sans-serif;}
+</style>
+<?php
+include_once("header.php");
+?>
+<body>
+<!-- Main content-->
+<div class="content-main" >
+    <img src="/consumer-advice/static/logo-SIT.png" alt="logo" width="100%" height="600">
+  <div class="w3-row w3-padding-64">
+    <div class="w3-twothird w3-container">
+      <h1 class="w3-text-teal">Welcome to our consumer advice portal!</h1>
+
+    </div>
+  </div>
+
+<?php
+include_once("footer.php");
+?>
+</div>
+</body>
+</html>
