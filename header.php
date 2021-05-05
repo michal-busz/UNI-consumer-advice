2ba28cba850b42f6519db5c45fdbf847efd9464a
Bilalhussai <80292469+Bilalhussai@users.noreply.github.com>
2021-05-05 13:47:15 +0100

    Update header.php

--- header.php
+++ header.php
@@ -1,9 +1,175 @@
-<div class="w3-top">
-    <div class="w3-bar theme w3-top w3-left-align w3-large">
-        <a class="w3-bar-item w3-button w3-right w3-hide-large w3-hover-white w3-large theme-l1" href="javascript:void(0)" onclick="w3_open()"><i class="fa fa-bars"></i></a>
-        <a href="/consumer-advice/index.php" class="w3-bar-item w3-button theme-l1">Home</a>
-        <a href="/consumer-advice/questions/questions.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Question And Answers</a>
-        <a href="/consumer-advice/about.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">About</a>
-        <a href="/consumer-advice/contact.php" class="w3-bar-item w3-button w3-hide-small w3-hover-white">Contact</a>
-    </div>
+!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+body {
+  margin: 0;
+  font-family: Arial, Helvetica, sans-serif;
+}
+
+.topnav {
+  overflow: hidden;
+  background-color: #333;
+}
+
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+.topnav a:hover {
+  background-color: orange;
+  color: black;
+}
+
+.topnav a.active {
+  background-color: #04AA6D;
+  color: white;
+}
+
+* {box-sizing: border-box;}
+body {font-family: Verdana, sans-serif;}
+.mySlides {display: none;}
+img {vertical-align: middle;}
+
+/* Slideshow container */
+.slideshow-container {
+  max-width: 1000px;
+  position: relative;
+  margin: auto;
+}
+
+/* Caption text */
+.text {
+  color: red;
+  font-size: 15px;
+  padding: 8px 12px;
+  position: absolute;
+  bottom: 8px;
+  width: 100%;
+  text-align: center;
+}
+
+
+}
+
+/* The dots/bullets/indicators */
+.dot {
+  height: 15px;
+  width: 15px;
+  margin: 0 2px;
+  background-color: #bbb;
+  border-radius: 50%;
+  display: inline-block;
+  transition: background-color 0.6s ease;
+}
+
+.active {
+  background-color: #717171;
+}
+
+/* Fading animation */
+.fade {
+  -webkit-animation-name: fade;
+  -webkit-animation-duration: 1.5s;
+  animation-name: fade;
+  animation-duration: 1.5s;
+}
+
+@-webkit-keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+@keyframes fade {
+  from {opacity: .4} 
+  to {opacity: 1}
+}
+
+/* On smaller screens, decrease text size */
+@media only screen and (max-width: 300px) {
+  .text {font-size: 11px}
+}
+</style>
+</head>
+<body>
+
+<div class="topnav">
+  <a class="" href="#home">Home</a>
+  <a href="">Start Quiz</a>
+  <a href="">Contact Us</a>
+  <a href="">About Us</a>
 </div>
+
+<div style="padding-left:30px">
+  <h2 style="color:blue;font-size:20;">Consumer Advice Help Portal</h2>
+  
+</div>
+
+
+
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+  
+  <img src="static/proofofpurchase.jpg" style="width:60%">
+  <div class="text">We are here to help you</div>
+</div>
+
+<div class="mySlides fade">
+  
+  <img src="static/clothesandfashionlogo.jpg" style="width:70%">
+  <div class="text"> Do you have an Issue with returning a Product?</div>
+</div>
+
+<div class="mySlides fade">
+ 
+  <img src="static/electrical-products.jpeg" style="width:100%">
+  <div class="text">  Do you have an Issue with returning a faulty Product?</div>
+</div>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+  <span class="dot"></span> 
+</div>
+
+<script>
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000);
+}
+</script>
+
+
+
+
+<footer>
+<p  style="color:white; background-color:red" = "copyright">  &COPY; COPYRIGHT 2021 LSBU Team 8 </p>
+</footer>
+
+</body>
+</html>
