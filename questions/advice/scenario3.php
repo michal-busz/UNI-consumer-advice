0fd857adac0cf3a0d0a0288f3b26a5c9de3f74c0
Michal Busz <michal@busz.it>
2021-05-11 16:58:19 +0100

    typo on scenario 3 fixed

--- questions/advice/scenario3.php
+++ questions/advice/scenario3.php
@@ -1,4 +1,4 @@
-<div class="content-main" style="margin-left:250px">1>
+<div class="content-main" style="margin-left:250px">
 <p>You have gone past your <b>first 6 months.</b><br>
 Your best course of action is to contact the seller. You should discuss the longevity of your product.<br>
 If so, you may still be entitled to a refund, repair, or replacement or exchange. It is unlikely, that if this is the case, the seller will give you a refund.<br> They are likely to give you a part refund, depending how much you have used the product.
